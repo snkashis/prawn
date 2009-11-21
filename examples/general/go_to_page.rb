a1cb6efcd16da97270c62fa0a9dafeac6527f405
James Healy <jimmy@deefa.com>
2009-11-21 22:44:05 +1100

    just use ints for to_to_page() - :first and :last is pointless, we can just use 0 and -1

--- examples/general/go_to_page.rb
+++ examples/general/go_to_page.rb
@@ -15,7 +15,7 @@ pdf.fill_color "00ff00"
 pdf.fill_polygon [100, 250], [200, 300], [300, 250],
                  [300, 150], [200, 100], [100, 150]            
 
-pdf.go_to_page(:first)
+pdf.go_to_page(0)
 pdf.fill_color "0000ff"
 pdf.fill_polygon [200, 350], [300, 400], [400, 350],
                  [400, 250], [300, 200], [200, 250]            
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -410,7 +410,7 @@ describe "The go_to_page() feature" do
     @pdf = Prawn::Document.new
     @pdf.line [100,100], [200,200]
     @pdf.start_new_page
-    @pdf.go_to_page(:first)
+    @pdf.go_to_page(0)
 
     output = StringIO.new(@pdf.render)
     hash = PDF::Hash.new(output)
@@ -425,7 +425,7 @@ describe "The go_to_page() feature" do
     @pdf.text "James"
     @pdf.start_new_page
     @pdf.text "Anthony"
-    @pdf.go_to_page(:first)
+    @pdf.go_to_page(0)
     @pdf.text "Healy"
 
     page_counter = PDF::Inspector::Page.analyze(@pdf.render)
@@ -437,7 +437,7 @@ describe "The go_to_page() feature" do
     @pdf.text "James"
     @pdf.start_new_page
     @pdf.text "Anthony"
-    @pdf.go_to_page(:first)
+    @pdf.go_to_page(0)
     @pdf.text "Healy"
 
     text = PDF::Inspector::Text.analyze(@pdf.render)
