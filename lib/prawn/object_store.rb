661787d693ac9408d41b917f25ebfd1668b076ba
James Healy <jimmy@deefa.com>
2009-11-21 23:19:37 +1100

    clean up ObjectStore template code a little

--- lib/prawn/object_store.rb
+++ lib/prawn/object_store.rb
@@ -18,13 +18,13 @@ module Prawn
       @objects = {}
       @identifiers = []
 
+      load_file(opts[:template]) if opts[:template]
+
       # Create required PDF roots
-      if opts[:template]
-        load_file(opts[:template])
-      else
-        @info    = ref(opts[:info] || {}).identifier
-        @pages   = ref(:Type => :Pages, :Count => 0, :Kids => [])
-        @root    = ref(:Type => :Catalog, :Pages => @pages).identifier
+      @info  ||= ref(opts[:info] || {}).identifier
+      @root  ||= ref(:Type => :Catalog).identifier
+      if pages.nil?
+        root.data[:Pages] = ref(:Type => :Pages, :Count => 0, :Kids => [])
       end
     end
 
@@ -81,6 +81,8 @@ module Prawn
 
     private
 
+    # takes a source PDF and uses it as a template for this document.
+    #
     def load_file(filename)
       unless File.file?(filename)
         raise ArgumentError, "#{filename} does not exist"
@@ -96,15 +98,10 @@ module Prawn
 
       if src_info
         @info = load_object_graph(hash, src_info).identifier
-      else
-        @info = ref({}).identifier
       end
 
       if src_root
         @root = load_object_graph(hash, src_root).identifier
-      else
-        @pages   = ref(:Type => :Pages, :Count => 0, :Kids => [])
-        @root    = ref(:Type => :Catalog, :Pages => @pages).identifier
       end
     rescue PDF::Reader::MalformedPDFError, PDF::Reader::InvalidObjectError
       msg = "Error reading template file. If you are sure it's a valid PDF, it may be a bug."
@@ -114,6 +111,12 @@ module Prawn
       raise Prawn::Errors::TemplateError, msg
     end
 
+    # recurse down an object graph from a source PDF, importing all the indirect
+    # objects we find.
+    #
+    # hash is the PDF::Hash to extract objects from, object is the object to
+    # extract.
+    #
     def load_object_graph(hash, object)
       @loaded_objects ||= {}
       case object
